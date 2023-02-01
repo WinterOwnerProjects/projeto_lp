3bf25eab0873f8e872cc423a7fbc7166b88bc3b4
WinterOwnerProjects <victor_duu@hotmail.com>
2023-02-01 16:36:32 -0300

    bloco6 finalizado

--- blocos/bloco6/bloco6.php
+++ blocos/bloco6/bloco6.php
@@ -1,34 +1,70 @@
-<style>
-    .div{
-        border: 2px solid black;
-        width: 300px;
-        height: 300px;
-        
-    }
-    .dive{
-        width: 300px;
-        height: 300px;
-        clip-path: circle(50% at 50% 50%);
-    }
-    img{
-        width: 100%;
-        height: 100%;
-    }
-</style>
-<div class='div'>
-    <div class='dive'><img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt=""></div>
-</div>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Taviraj:ital,wght@0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/projeto_lp/css/css_bloco_6/style.css">
 
-<div>
-    <div class="conteudo1-depoimento">
-        <div>imagem</div>
-        <div>nome e cargo</div>
+<section class="section6a">
+    <h2>What <span>people say</span></h2>
+    <div class="section6b">
+        <div>
+            <div class="conteudo-depoimento">
+                <div class="conteudo1-depoimento flex-content">
+                    <div class='clip-path'>
+                        <img class="depoiment-img" src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt="">
+                    </div>
+                    <div class="person-name-div1">
+                    </div>
+                    <div class="person-name-div2">
+                        <p><b>Annette Black</b></p>
+                        <br>
+                        <p><span>Custumer Success</span></p>
+                    </div>
+                </div>
+                <hr class="depoiment-hr">
+                <div class="conteudo2-depoimento flex-content">
+                    <div class="person-name-div1"></div>
+                    <div class="person-name-div2b">
+                        <img src="/projeto_lp/imagens/icons/facebook.png" alt="">
+                        <img src="/projeto_lp/imagens/icons/instagram.png" alt="">
+                        <img src="/projeto_lp/imagens/icons/linkedin.png" alt="">
+                    </div>
+                </div>
+                <div class="conteudo3-depoimento">
+                    <div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="conteudo-depoimento">
+                <div class="conteudo1-depoimento flex-content">
+                    <div class='clip-path'>
+                        <img class="depoiment-img" src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt="">
+                    </div>
+                    <div class="person-name-div1">
+                    </div>
+                    <div class="person-name-div2">
+                        <p><b>Annette Black</b></p>
+                        <br>
+                        <p><span>Custumer Success</span></p>
+                    </div>
+                </div>
+                <hr class="depoiment-hr">
+                <div class="conteudo2-depoimento flex-content">
+                    <div class="person-name-div1"></div>
+                    <div class="person-name-div2b">
+                        <img src="/projeto_lp/imagens/icons/facebook.png" alt="">
+                        <img src="/projeto_lp/imagens/icons/instagram.png" alt="">
+                        <img src="/projeto_lp/imagens/icons/linkedin.png" alt="">
+                    </div>
+                </div>
+                <div class="conteudo3-depoimento">
+                    <div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="conteudo2-depoimento">
-        <div>bloco em branco</div>
-        <div>icons redes sociais</div>
-    </div>
-    <div class="conteudo3-depoimento">
-        <div>texto do depoimento</div>
-    </div>
-</div>
+</section>
